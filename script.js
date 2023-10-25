d17c5030154ffecbf265c32150c15d1662fdca28
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-10-25 23:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let form=document.getElementById("form1");
+	let firstInput=form.elements["fname"];
+	let lastInput=form.elements["lname"];
 
+	let firstname=firstInput.value;
+	let lastname=lastInput.value;
+	alert(firstname+" "+lastname);
 }
